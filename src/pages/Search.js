2d4f25133af0ff8a0f40850137410e25f92c4fd9
Marcelo De Lima <delima_marcelo@outlook.com>
2022-05-19 10:27:32 -0300

    Exercicio 05 Finalizado

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -2,11 +2,36 @@ import React from 'react';
 import Header from '../Components/Header';
 
 class Search extends React.Component {
+  state = {
+    artista: '',
+  };
+
+  handleChange = (event) => {
+    this.setState({ artista: event.target.value });
+  };
+
   render() {
+    const { artista } = this.state;
+    const CHAVE_BUTTON = 2;
     return (
       <div data-testid="page-search">
         <Header />
         <h1>Search</h1>
+        <form>
+          <input
+            type="text"
+            data-testid="search-artist-input"
+            placeholder="Banda / Artista"
+            onChange={ this.handleChange }
+          />
+          <button
+            type="button"
+            data-testid="search-artist-button"
+            disabled={ artista.length < CHAVE_BUTTON }
+          >
+            Pesquisar
+          </button>
+        </form>
       </div>
     );
   }
