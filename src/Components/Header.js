e40bb080a16a568f15d4036a3b2da5b91083438e
Marcelo De Lima <delima_marcelo@outlook.com>
2022-05-19 09:27:23 -0300

    Exercicio 04 Finalizado

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -20,9 +21,16 @@ class Header extends React.Component {
     if (load) return <Loading />;
     return (
       <header data-testid="header-component">
-        <h2 data-testid="header-user-name">
-          {user.name}
-        </h2>
+        <h2 data-testid="header-user-name">{user.name}</h2>
+        <hr />
+        <nav>
+          <Link to="/search" data-testid="link-to-search">Search</Link>
+          <br />
+          <Link to="/favorites" data-testid="link-to-favorites">Favorites</Link>
+          <br />
+          <Link to="/profile" data-testid="link-to-profile">Profile</Link>
+          <br />
+        </nav>
       </header>
     );
   }
