d08c6e7d3d567aac23d26f9691a4bd9adc0da50d
Marcelo De Lima <delima_marcelo@outlook.com>
2022-05-19 21:50:11 -0300

    Exercicio 08 Finalizado

--- src/Components/MusicCard.js
+++ src/Components/MusicCard.js
@@ -1,10 +1,27 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import { addSong } from '../services/favoriteSongsAPI';
+import Loading from './Loading';
 
 class MusicCard extends React.Component {
+  state = {
+    loading: false,
+  };
+
+  favorite = async () => {
+    const { music } = this.props;
+    this.setState({ loading: true }, async () => {
+      await addSong(music);
+      this.setState({
+        loading: false,
+      });
+    });
+  };
+
   render() {
     const { music } = this.props;
-    const { trackName, previewUrl } = music;
+    const { trackName, previewUrl, trackId } = music;
+    const { loading } = this.state;
     return (
       <ul>
         <li>
@@ -15,7 +32,17 @@ class MusicCard extends React.Component {
             O seu navegador não suporta o elemento
             <code>audio</code>
           </audio>
+          <label htmlFor="fav">
+            Favorita
+            <input
+              name="fav"
+              type="checkbox"
+              onClick={ this.favorite }
+              data-testid={ `checkbox-music-${trackId}` }
+            />
+          </label>
         </li>
+        {loading && <Loading />}
       </ul>
     );
   }
@@ -25,6 +52,7 @@ MusicCard.propTypes = {
   music: PropTypes.shape({
     trackName: PropTypes.string.isRequired,
     previewUrl: PropTypes.string.isRequired,
+    trackId: PropTypes.number.isRequired,
   }).isRequired,
 };
 
